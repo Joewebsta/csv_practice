3a8778a070331ab07fe2c2896519a007906bbfb7
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-09 13:32:17 -0400

    Convert each row into an instance of MagicalPet.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require './lib/animal_lover'
+require './lib/magical_pet'
 
 CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row|
   id = row[:id].to_i
@@ -10,4 +11,10 @@ CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbo
   p AnimalLover.new(id, first_name, last_name, age)
 end
 
-CSV.foreach('./data/magical_pets.csv', headers: true, header_converters: :symbol) { |row| p row }
+CSV.foreach('./data/magical_pets.csv', headers: true, header_converters: :symbol) do |row|
+  id = row[:id].to_i
+  name = row[:name]
+  pet_owner_id = row[:pet_owner_id].to_i
+
+  p MagicalPet.new(id, name, pet_owner_id)
+end
