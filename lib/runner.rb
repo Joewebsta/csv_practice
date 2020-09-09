aeece1eaf6adf567c45c04c3f565bf0a23d66bd7
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-09 13:20:03 -0400

    Convert each row into an instance of AnimalLover

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,5 +1,13 @@
 require 'csv'
+require './lib/animal_lover'
 
-CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) { |row| p row }
+CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row|
+  id = row[:id].to_i
+  first_name = row[:first_name]
+  last_name = row[:last_name]
+  age = row[:age]
+
+  p AnimalLover.new(id, first_name, last_name, age)
+end
 
 CSV.foreach('./data/magical_pets.csv', headers: true, header_converters: :symbol) { |row| p row }
